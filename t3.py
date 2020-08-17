1cc447a10ab73106e7018e045e39c501668052ea
Johnny Tirado <johnny.tirado.ii@gmail.com>
2020-08-16 21:14:24 -0400

    Infinite loop fixed, now able to draw an X.

--- t3.py
+++ t3.py
@@ -24,28 +24,32 @@ def display_board():
 
 # We need to ask the player's input
 def request_move():
-	print('Please input a square. You\'re playing as X. They are:')
-	print('ul|uc|ur')
-	print('--------')
-	print('ml|mc|mr')
-	print('--------')
-	print('ll|lc|lr')
+    print('Please input a square. You\'re playing as X. They are:')
+    print('ul|uc|ur')
+    print('--------')
+    print('ml|mc|mr')
+    print('--------')
+    print('ll|lc|lr')
 
-	proceed = False
-	while proceed == False:
-		req_square = input('Input a square: ')
-		print('Debug: ' + req_square)
-		invalid = validate_input(req_square)	
+    proceed = False
+    while proceed == False:
+        req_square = input('Input a square: ')
+        print('Debug: ' + req_square)
+        proceed = validate_input(req_square)	
+    mark_board(req_square)
 
 # We need to take that input and make sure that they're making an actual move.
 def validate_input(i):
-	valid_inputs = ('ul','uc','ur','ml','mc','mr','ll','lc','lr')
-	if i in valid_inputs:
-		return True
-	else:
-		return False
+    valid_inputs = ['ul','uc','ur','ml','mc','mr','ll','lc','lr']
+    if i in valid_inputs:
+        return True
+    else:
+        return False
 
 # We need to put that move on the board.
+def mark_board(var_space):
+    board[var_space] = "X"
+
 # We need to show this board to the player
 # We need to confirm if win or draw. End game if so and state who won if anyone
 # We need to determine a move. This is prioritized by: scoring a win, avoiding a loss, or a random move.
@@ -56,3 +60,5 @@ def validate_input(i):
 initialize_board()
 display_board()
 request_move()
+display_board()
+
