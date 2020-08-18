a6516953f66dc2746e7098bbd8e71dcc71a957d6
Johnny Tirado <johnny.tirado.ii@gmail.com>
2020-08-17 21:17:37 -0400

    Fixed issue #2, added validation of wins

--- t3.py
+++ t3.py
@@ -2,27 +2,25 @@
 
 import os
 
-# We need to draw a board
 def initialize_board():
 	global board
 	board = {
 		'ul': ' ', 'uc': ' ', 'ur': ' ',
 		'ml': ' ', 'mc': ' ', 'mr': ' ',
-		'll': ' ', 'lc': ' ', 'lr': ' '
+		'll': 'X', 'lc': 'X', 'lr': 'X'
 	}
 
-# We need to show this board to the player
 def display_board():
-	os.system('clear')
-	print('Current board is:')
-	print(board['ul'] + '|' + board['uc'] + '|' + board['ur'])
-	print('-----')
-	print(board['ml'] + '|' + board['mc'] + '|' + board['mr'])
-	print('-----')
-	print(board['ll'] + '|' + board['lc'] + '|' + board['lr'])
-	print()
+    os.system('clear')
+    print('Current board is:')
+    print(board['ul'] + '|' + board['uc'] + '|' + board['ur'])
+    print('-----')
+    print(board['ml'] + '|' + board['mc'] + '|' + board['mr'])
+    print('-----')
+    print(board['ll'] + '|' + board['lc'] + '|' + board['lr'])
+    print()
 
-# We need to ask the player's input
+# Prompt for input. If valid, mark board.
 def request_move():
     print('Please input a square. You\'re playing as X. They are:')
     print('ul|uc|ur')
@@ -36,22 +34,73 @@ def request_move():
         req_square = input('Input a square: ')
         print('Debug: ' + req_square)
         proceed = validate_input(req_square)	
-    mark_board(req_square)
+    mark_board(req_square, "X")
 
-# We need to take that input and make sure that they're making an actual move.
+# Confirm move is valid. Checks against position and if slot is empty.
 def validate_input(i):
     valid_inputs = ['ul','uc','ur','ml','mc','mr','ll','lc','lr']
     if i in valid_inputs:
-        return True
+        if board[i] == " ":
+            return True
+        else:
+            return False
     else:
         return False
 
-# We need to put that move on the board.
-def mark_board(var_space):
-    board[var_space] = "X"
+# Take a position and a marker and mark the board appropriately.
+def mark_board(var_space, player_marker):
+    board[var_space] = player_marker
 
-# We need to show this board to the player
-# We need to confirm if win or draw. End game if so and state who won if anyone
+# Return True if X won, False if not.
+def check_win_x():
+    # Horizontals
+    if board["ul"] == "X" and board["uc"] == "X" and board["ur"] == "X":
+        return True
+    elif board["ml"] == "X" and board["mc"] == "X" and board["mr"] == "X":
+        return True
+    elif board["ll"] == "X" and board["lc"] == "X" and board["lr"] == "X":
+        return True
+    # Verticals
+    elif board["ul"] == "X" and board["ml"] == "X" and board["ll"] == "X":
+        return True
+    elif board["uc"] == "X" and board["mc"] == "X" and board["lc"] == "X":
+        return True
+    elif board["ur"] == "X" and board["mr"] == "X" and board["lr"] == "X":
+        return True
+    # Diags
+    elif board["ul"] == "X" and board["mc"] == "X" and board["lr"] == "X":
+        return True
+    elif board["ll"] == "X" and board["mc"] == "X" and board["ur"] == "X":
+        return True
+    # No win
+    else:
+        return False
+
+# Return True if O won, False if not.
+def check_win_o():
+    # Horizontals
+    if board["ul"] == "O" and board["uc"] == "O" and board["ur"] == "O":
+        return True
+    elif board["ml"] == "O" and board["mc"] == "O" and board["mr"] == "O":
+        return True
+    elif board["ll"] == "O" and board["lc"] == "O" and board["lr"] == "O":
+        return True
+    # Verticals
+    elif board["ul"] == "O" and board["ml"] == "O" and board["ll"] == "O":
+        return True
+    elif board["uc"] == "O" and board["mc"] == "O" and board["lc"] == "O":
+        return True
+    elif board["ur"] == "O" and board["mr"] == "O" and board["lr"] == "O":
+        return True
+    # Diags
+    elif board["ul"] == "O" and board["mc"] == "O" and board["lr"] == "O":
+        return True
+    elif board["ll"] == "O" and board["mc"] == "O" and board["ur"] == "O":
+        return True
+    # No win
+    else:
+        return False
+
 # We need to determine a move. This is prioritized by: scoring a win, avoiding a loss, or a random move.
 # We need to put that move on the board.
 # We need to show this board to the player
@@ -61,4 +110,5 @@ initialize_board()
 display_board()
 request_move()
 display_board()
-
+check_win_debug = check_win_x()
+print(str(check_win_debug))
