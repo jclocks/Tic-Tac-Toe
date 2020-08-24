2dd8d67c705ab4801ed4c0d602ec2b87ef6cab71
Johnny Tirado <johnny.tirado.ii@gmail.com>
2020-08-24 08:47:55 -0400

    Game complete as 2 player only.

--- t3.py
+++ t3.py
@@ -2,128 +2,120 @@
 
 import os
 
-# Rework of board variable.
-def initialize_board():
-	global board
-	board = "         "
+board = {
+    'ul': ' ', 'uc': ' ', 'ur': ' ',
+    'ml': ' ', 'mc': ' ', 'mr': ' ',
+    'll': ' ', 'lc': ' ', 'lr': ' '
+}
+
+x_turn = True
 
 def display_board():
-    os.system('clear')
     print('Current board is:')
-    print(board[0] + '|' + board[1] + '|' + board[2])
-    print('-----')
-    print(board[3] + '|' + board[4] + '|' + board[5])
-    print('-----')
-    print(board[6] + '|' + board[7] + '|' + board[8])
+    print(board['ul'] + '|' + board['uc'] + '|' + board['ur'])
+    print('--------')
+    print(board['ml'] + '|' + board['mc'] + '|' + board['mr'])
+    print('--------')
+    print(board['ll'] + '|' + board['lc'] + '|' + board['lr'])
     print()
 
-# Prompt for input. If valid, mark board.
-def request_move():
-    print('Please input a square. You\'re playing as ' + player_choice + '. They are:')
-    print('0|1|2')
-    print('-----')
-    print('3|4|5')
+def display_help():
+    print('The available squares are:')
+    print('ul|uc|ur')
     print('--------')
-    print('6|7|8')
+    print('ml|mc|mr')
+    print('--------')
+    print('ll|lc|lr')
+    print()
 
-    proceed = False
-    while proceed == False:
-        req_square = input('Input a square: ')
-        proceed = validate_input(int(req_square))	
-    mark_board(int(req_square), player_choice)
+def move_validation(chosen_square):
+    return True if (chosen_square in ['ul', 'uc', 'ur', 'ml', 'mc', 'mr', 'll', 'lc', 'lr']) and (board[chosen_square] == ' ') else False
 
-# Confirm move is valid. Checks against position and if slot is empty.
-def validate_input(i):
-    if (board[i] == " "):
-        print(True)
-        return True
+def mark_board(chosen_square):
+    global x_turn
+    if x_turn == True:
+        board[chosen_square] = 'X'
     else:
-        print(False)
-        return False
+        board[chosen_square] = 'O'
+    x_turn = not x_turn
 
-# Take a position and a marker and mark the board appropriately.
-def mark_board(var_space, player_marker):
-    board[var_space] = player_marker
-
-# Return True if X won, False if not.
-def check_win_x():
+def game_over():
     # Horizontals
-    if board[0] == board[1] == board[2] == "X":
+    if board['ul'] == board['uc'] == board['ur'] == 'X':
+        print('X wins!')
         return True
-    elif board[3] == board[4] == board[5] == "X":
+    elif board['ml'] == board['mc'] == board['mr'] == 'X':
+        print('X wins!')
         return True
-    elif board[6] == board[7] == board[8] == "X":
+    elif board['ll'] == board['lc'] == board['lr'] == 'X':
+        print('X wins!')
+        return True
+    elif board['ul'] == board['uc'] == board['ur'] == 'O':
+        print('O wins!')
+        return True
+    elif board['ml'] == board['mc'] == board['mr'] == 'O':
+        print('O wins!')
+        return True
+    elif board['ll'] == board['lc'] == board['lr'] == 'O':
+        print('O wins!')
         return True
     # Verticals
-    elif board[0] == board[3] == board[6] == "X":
+    elif board['ul'] == board['ml'] == board['ll'] == 'X':
+        print('X wins!')
         return True
-    elif board[1] == board[4] == board[7] == "X":
+    elif board['uc'] == board['mc'] == board['lc'] == 'X':
+        print('X wins!')
         return True
-    elif board[2] == board[5] == board[8] == "X":
+    elif board['ur'] == board['mr'] == board['lr'] == 'X':
+        print('X wins!')
         return True
-    # Diags
-    elif board[0] == board[4] == board[8] == "X":
+    elif board['ul'] == board['ml'] == board['ll'] == 'O':
+        print('O wins!')
         return True
-    elif board[6] == board[4] == board[2] == "X":
+    elif board['uc'] == board['mc'] == board['lc'] == 'O':
+        print('O wins!')
         return True
-    # No win
+    elif board['ur'] == board['mr'] == board['lr'] == 'O':
+        print('O wins!')
+        return True
+    # Diagonals
+    elif board['ul'] == board['mc'] == board['lr'] == 'X':
+        print('X wins!')
+        return True
+    elif board['ll'] == board['mc'] == board['ur'] == 'X':
+        print('X wins!')
+        return True
+    elif board['ul'] == board['mc'] == board['lr'] == 'O':
+        print('O wins!')
+        return True
+    elif board['ll'] == board['mc'] == board['ur'] == 'O':
+        print('O wins!')
+        return True
+    # Draw Game
+    elif ' ' not in board.values():
+        print('Draw game.')
+        return True
+    # Not Over
     else:
         return False
 
-# Return True if O won, False if not.
-def check_win_o():
-    # Horizontals
-    if board[0] == board[1] == board[2] == "O":
-        return True
-    elif board[3] == board[4] == board[5] == "O":
-        return True
-    elif board[6] == board[7] == board[8] == "O":
-        return True
-    # Verticals
-    elif board[0] == board[3] == board[6] == "O":
-        return True
-    elif board[1] == board[4] == board[7] == "O":
-        return True
-    elif board[2] == board[5] == board[8] == "O":
-        return True
-    # Diags
-    elif board[0] == board[4] == board[8] == "O":
-        return True
-    elif board[6] == board[4] == board[2] == "O":
-        return True
-    # No win
+# Game logic:
+while game_over() == False:
+    # Clear the screen.
+    os.system('clear')
+    # State whose turn it is.
+    if x_turn == True:
+        print('It\'s currently X\'s turn.')
     else:
-        return False
-
-def check_draw():
-    if ' ' not in board.values():
-        return True
-    else:
-        return False
-
-# Select a game mode.
-def initialize_game():
-    global player_choice, opponent_type
-    player_choice, opponent_type = "", ""
-    while (player_choice != "X") and (player_choice != "O"):
-        player_choice = input("Please choose X or O: ")
-        player_choice = player_choice.upper()
-    while (opponent_type != "P") and (opponent_type != "C"):
-        opponent_type = input("Please type P to play another human, or C for the computer: ")
-        opponent_type = opponent_type.upper()
-    initialize_board()
-
-# Game flow
-initialize_game()
-game_over = False
-while game_over == False:
+        print('It\'s currently O\'s turn.')
+    # Display the board and help.
     display_board()
-    request_move()
-    if (check_win_x() == True) or (check_win_o() == True) or (check_draw() == True):
-        game_over = True
-if check_win_x() == True:
-    print("X wins!")
-elif check_win_o() == True:
-    print("O wins!")
-else:
-    print("Draw game!")
+    display_help()
+    # Prompt for a square until a valid one is given.
+    move_valid = False
+    while move_valid == False:
+        player_choice = input('Please make a move from the above squares: ')
+        move_valid = move_validation(player_choice)
+    # Mark the board / flip turn.
+    mark_board(player_choice)
+    # Loop handles exiting if the game's over.
